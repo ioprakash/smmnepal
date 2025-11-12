e430d4d3f6e6c29c85b8e5a89aca3a01f1bc5b3c
Prakash Kushwaha <prakash.181529@ncit.edu.np>
2025-11-12 23:48:34 +0545

    first commit

---
+++ app/cn.php
@@ -0,0 +1,25 @@
+
+<?php
+define('PATH', realpath('.'));
+define('SUBFOLDER', false);
+define('URL', 'https://project.smmscripts.com');
+define('STYLESHEETS_URL', '//project.smmscripts.com');
+date_default_timezone_set('Asia/Kolkata');
+
+/* 
+ ini_set("display_errors","1");
+error_reporting(E_ERROR);  */  
+
+error_reporting(0);
+return [
+  'db' => [
+    'name'    =>  'u650314999_project',
+    'host'    =>  'localhost',
+    'user'    =>  'u650314999_project',
+    'pass'    =>  'gbUe6^S1>',
+    'charset' =>  'utf8mb4'
+  ]
+];
+
+
+?>
